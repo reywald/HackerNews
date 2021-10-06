2d6e811c20341b2467e20c0d3d591d79e1d9e5a6
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2021-10-07 00:03:00 +0100

    Add models to admin and create super user

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comment, Job, Story, Poll, PollOption
 
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Job)
+admin.site.register(Story)
+admin.site.register(Poll)
+admin.site.register(PollOption)
