ac937d99e9e6276120dad54dcf580e336b78b674
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2021-10-07 01:12:47 +0100

    Fetch data from API
    Configure the view, templates and url to display 100 news items

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import NewsListView
+from .views import homePageView
 
 urlpatterns = [
-    # path('', view=None, name=''),
+    path('', homePageView, name='home'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,21 @@
+from django import shortcuts
+
+
 from django.shortcuts import render
+from django.http import HttpResponse, response
+from django.views.generic import ListView
+
+import requests, json
 
 # Create your views here.
+
+class NewsListView(ListView):
+    pass
+
+def homePageView(request):
+    # get the list of news items
+    url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    response = requests.get(url=url)
+    news_items = response.json()
+    
+    return render(request, 'home.html', {"news_items": news_items[:100]})
